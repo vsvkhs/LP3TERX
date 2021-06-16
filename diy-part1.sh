31c01f9d174ca5fa05bb835a4b8f20b7c959b1b1
vsvkhs <61653054+vsvkhs@users.noreply.github.com>
2021-06-17 04:09:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
